a5ba9d2229a8de662acbcfabed46c033b2546103
Silas Matos <silas.progamacao@gmail.com>
2025-03-26 09:53:22 -0300

    .

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+// src/app.ts
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,34 +12,38 @@ const database_1 = require("./configs/database");
 class App {
     constructor() {
         this.server = (0, fastify_1.default)();
-        this.server.register(cors_1.default, {
+        this.setup();
+    }
+    async setup() {
+        await this.registerPlugins();
+        this.routes();
+        await this.connectToDb();
+    }
+    async registerPlugins() {
+        await this.server.register(cors_1.default, {
             origin: ['http://localhost:5173', 'http://localhost:3001']
         });
-        this.routes();
-        this.connectToDb();
     }
     routes() {
         this.server.get('/', this.index);
         new Router_1.MainRoutes(this.server);
     }
     async index(request, reply) {
-        reply.send({ message: 'Seja, bem vindo' });
+        reply.send({ message: '🚀 API Online: Seja bem-vindo!' });
     }
     async connectToDb() {
         await (0, database_1.connectToDb)();
     }
-    start() {
-        const port = process.env.PORT || 3000;
-        this.server.listen({
-            port: Number(port),
-            host: '0.0.0.0'
-        }, (err, address) => {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            console.log(`Server is running on ${address} ✅`);
-        });
+    async start() {
+        const port = Number(process.env.PORT || 3000);
+        try {
+            await this.server.listen({ port, host: '0.0.0.0' });
+            console.log(`✅ Server is running on http://localhost:${port}`);
+        }
+        catch (err) {
+            console.error('❌ Failed to start server:', err);
+            process.exit(1);
+        }
     }
 }
 const app = new App();
